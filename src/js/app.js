b38f381f7a3f9928ce22088071e81dfc6a3a772f
Vasil <vasilmuktasipov@gmail.com>
2023-12-05 23:33:40 +0400

    JS code refactoring. Optimization of translateZ calculations

--- src/js/app.js
+++ src/js/app.js
@@ -1,204 +1,197 @@
-let main = document.querySelector('main');
-let loader = document.querySelector('.loader');
+const Z_CHANGE_SPEED_MULTIPLIER = -5;
+const Z_DEPTH_DISTANCE = -1000;
+const OPACITY_TOOGLE_DISTACE = Math.abs(Z_DEPTH_DISTANCE) / 1.8;
+const PORTFOLIO_GALARY_VISIBILITY_RANGE = Z_DEPTH_DISTANCE * 6.5;
+const FRAME_VISIBILITY_RANGE = Z_DEPTH_DISTANCE * 6;
+const BEYOND_SCREEN_VISIBILITY_RANGE = Math.abs(Z_DEPTH_DISTANCE) * 3;
+
+const main = document.querySelector('main');
+const loader = document.querySelector('.loader');
+const bodyPage = document.querySelector('body');
+const navLinks = document.querySelectorAll('.main-nav__item > a');
+
+const logoSpan = document.querySelector('.logo__span');
+const mainNav = document.querySelector('.main-nav');
+const menuButton = document.querySelector('.menu-button');
+const pageFooter = document.querySelector('.page-footer');
+const portfolioGalleryList = document.querySelector('.portfolio-list');
 
 let frames = document.querySelectorAll('.frame-box');
-const bodyPage = document.querySelector('body');
+let portfolioFrameIndex = frames.length - 1;
+let portfolioFrame = frames[portfolioFrameIndex];
 
-const zDepthDistance = -1000;
-// множитель на прокрутку transform по оси Z по сравнению с прокруткой scroll в body (скорость z скролла)
-const zChangeSpeedMultiplier = -5;
-let zDistancesArray = [];
-
-
-window.onload = function () {
-   setBodyHeight();
-   setFramePortrait();
-
-   for (let i = 0; i < frames.length; i++) {
-      zDistancesArray.push(i * zDepthDistance);
-   }
-   // последний элемент стоит дальше, чтоб предыдущий не перекрывал интерактив draggable в portfolio-gallery
-   zDistancesArray[frames.length - 1] -= 1000;
-
-   const portfolioGalleryList = document.querySelector('.portfolio-list');
-   const portfolioGalleryFrame = document.querySelector('.frame-box:last-of-type');
-
-   Draggable.create(portfolioGalleryList, {
-      bounds: portfolioGalleryFrame,
-      allowNativeTouchScrolling: true,
-      allowEventDefault: true
-   })
-}
-
-
-window.onresize = function () {
-   setBodyHeight();
-}
+let zDistances = [];
+let lastScrollPosition = 0;
 
 
 function setBodyHeight() {
-   let viewportHeight = window.innerHeight;
+   const viewportHeight = window.innerHeight;
    // "+ высота viewport" зафиксировать последний фрейм на z~0px, "+ 1" костыль draggable для мобильных устройств
-   let bodyHeight = (frames.length * zDepthDistance / zChangeSpeedMultiplier) + viewportHeight + 1;
+   const bodyHeight = (frames.length * Z_DEPTH_DISTANCE / Z_CHANGE_SPEED_MULTIPLIER) + viewportHeight + 1;
 
-   bodyPage.style.setProperty('--z-depth', bodyHeight + 'px');
+   bodyPage.style.setProperty('--z-depth', `${bodyHeight}px`);
 }
-
 
 function setFramePortrait() {
    const mediaQueryTablet = window.matchMedia('(max-width: 768px)');
    const mediaQueryPortrait = window.matchMedia('(orientation: portrait)');
 
    if (mediaQueryTablet.matches && mediaQueryPortrait.matches) {
-      let framePortraits = document.querySelectorAll('.frame-box_portrait');
-      framePortraits.forEach(function (framePortrait) {
-         framePortrait.classList.add('frame-box');
-      });
+      const framePortraits = document.querySelectorAll('.frame-box_portrait');
+
+      framePortraits.forEach((framePortrait) => framePortrait.classList.add('frame-box'));
+
       frames = document.querySelectorAll('.frame-box');
+      portfolioFrameIndex = frames.length - 1;
+      portfolioFrame = frames[portfolioFrameIndex];
    }
 }
 
+function setFrames() {
+   setFramePortrait();
+   setBodyHeight();
 
-let lastScrollPosition = 0;
+   for (let i = 0; i < frames.length; i++) {
+      zDistances.push(i * Z_DEPTH_DISTANCE);
+   }
 
-window.onscroll = function () {
+   // последний элемент стоит дальше, чтоб предыдущий не перекрывал интерактив draggable в portfolio-gallery
+   zDistances[portfolioFrameIndex] += Z_DEPTH_DISTANCE;
+
+   Draggable.create(portfolioGalleryList, {
+      bounds: document.querySelector('.frame-box:last-of-type'),
+      allowNativeTouchScrolling: true,
+      allowEventDefault: true
+   });
+}
+
+function removeLoader() {
+   // запуск scroll логики
+   window.scrollTo(0, 1);
+   window.scrollTo(0, 0);
+
+   setTimeout(() => {
+      loader.style.display = 'none';
+      main.style.opacity = 1;
+   }, 1000);
+}
+
+window.addEventListener('load', setFrames, { once: true });
+window.addEventListener('load', removeLoader, { once: true });
+window.addEventListener('resize', setBodyHeight);
+
+
+function runScrollLogic() {
    let scrollTop = document.documentElement.scrollTop;
    let positionsDelta = lastScrollPosition - scrollTop;
    lastScrollPosition = scrollTop;
 
-   frames.forEach(function (frame, i, frames) {
-      let portfolioFrameIndex = frames.length - 1;
-      let isNotPortfolioFrame = i !== frames.length - 1;
-      let isNotMaskedScreenFrame = i !== frames.length - 2;
+   frames.forEach((frame, i) => {
+      let translateZ = zDistances[i] += positionsDelta * Z_CHANGE_SPEED_MULTIPLIER;
 
-      zDistancesArray[i] += positionsDelta * zChangeSpeedMultiplier;
+      const isMaskedScreenFrame = i === frames.length - 2;
+      const isPortfolioFrame = i === portfolioFrameIndex;
+      const isMaskedImageFrame = frame.hasAttribute('data-masked-image');
+      const isMaskRunRange = translateZ >= Z_DEPTH_DISTANCE;
+      const isTextInVisibleRange = translateZ >= 0;
+      const isPortfolioFrameInVisibleRange = zDistances[portfolioFrameIndex] >= PORTFOLIO_GALARY_VISIBILITY_RANGE;
+      const isFrameInVisibleRange = translateZ >= FRAME_VISIBILITY_RANGE;
+      const isFrameInBeyondScreenRange = translateZ <= BEYOND_SCREEN_VISIBILITY_RANGE;
 
-      let opacity = zDistancesArray[i] < Math.abs(zDepthDistance) / 1.8 ? 1 : 0;
-      let zTransform = `translateZ(${zDistancesArray[i]}px)`;
+      const opacity = (translateZ < OPACITY_TOOGLE_DISTACE && isFrameInVisibleRange) ? 1 : 0;
 
-      if (isNotPortfolioFrame && isNotMaskedScreenFrame) {
-         frame.setAttribute('style', `transform: ${zTransform}; opacity: ${opacity};`);
+      frame.style.opacity = !isMaskedScreenFrame ? opacity : 1;
+
+      if (
+         isFrameInVisibleRange
+         && isFrameInBeyondScreenRange
+         || isPortfolioFrameInVisibleRange
+      ) {
+         frame.style.transform = `translateZ(${translateZ}px)`;
+      } else if (!isFrameInBeyondScreenRange) {
+         frame.style.transform = `translateZ(${BEYOND_SCREEN_VISIBILITY_RANGE}px)`;
+      } else if (isPortfolioFrame && !isPortfolioFrameInVisibleRange) {
+         portfolioFrame.style.opacity = 0;
+         frame.style.transform = `translateZ(${PORTFOLIO_GALARY_VISIBILITY_RANGE}px)`;
       } else {
-         frame.setAttribute('style', `transform: ${zTransform};`);
+         frame.style.transform = `translateZ(${FRAME_VISIBILITY_RANGE}px)`;
       }
 
+      if (isMaskedImageFrame && isMaskRunRange) frame.classList.add('frame-box_mask-active');
 
-      let isPortfolioFrame = i === frames.length - 1;
-      let portfolioGalleryVisibleRange = zDistancesArray[portfolioFrameIndex] >= zDepthDistance * 6.5;
-      let framesVisibleRange = zDistancesArray[i] >= zDepthDistance * 6;
-      let frameBeyondScreenRange = zDistancesArray[i] <= Math.abs(zDepthDistance) * 3;
-
-      if (isPortfolioFrame) {
-         portfolioGalleryVisibleRange ?
-            frame.classList.remove('hidden') : frame.classList.add('hidden');
-      } else {
-         (framesVisibleRange && frameBeyondScreenRange) ?
-            frame.classList.remove('hidden') : frame.classList.add('hidden');
-      }
-
-
-      let isMaskedImageFrame = frame.hasAttribute('data-masked-image');
-      let isMaskRunRange = zDistancesArray[i] >= zDepthDistance;
-
-      if (isMaskedImageFrame && isMaskRunRange) {
-         frame.classList.add('frame-box_mask-active');
-      }
-
-
-      let portfolioFrame = frames[portfolioFrameIndex];
-      let isTextVisibleRange = zDistancesArray[i] >= 0;
-
-      if (portfolioFrame && isTextVisibleRange) {
-         portfolioFrame.style.setProperty('--_text-opacity', 1);
-      }
+      if (portfolioFrame && isTextInVisibleRange) portfolioFrame.style.setProperty('--_text-opacity', 1);
    });
 }
 
+window.addEventListener('scroll', runScrollLogic);
 
-const navLinks = document.querySelectorAll('.main-nav__item > a');
 
-navLinks.forEach(function (navLink) {
-   navLink.onclick = function () {
-      navScrollData.scrollToFrame(navLink);
-   }
-})
+const navScrollData = {
+   dataSectionNames: [
+      'data-portfolio-works',
+      'data-about',
+      'data-team',
+      'data-concepts',
+      'data-portfolio-gallery',
+   ],
 
-let navScrollData = {
-   dataNames: ['data-portfolio-works', 'data-about', 'data-team', 'data-concepts', 'data-portfolio-gallery'],
-
-   scrollToFrame: function (navLink) {
-      for (let i = 0; i < this.dataNames.length; i++) {
-         if (navLink.hasAttribute(this.dataNames[i])) {
-            window.scrollTo(0, this.getTopScrollHeight(this.dataNames[i]));
+   scrollToFrame(navLink) {
+      for (const sectionName of this.dataSectionNames) {
+         if (navLink.hasAttribute(sectionName)) {
+            window.scrollTo(0, this.getTopScrollHeight(sectionName));
          }
       }
    },
 
-   getTopScrollHeight: function (DataName) {
+   getTopScrollHeight(dataName) {
       for (let i = 0; i < frames.length; i++) {
-         let isTheLastFrame = i === frames.length - 1;
+         const isTheLastFrame = i === portfolioFrameIndex;
 
-         if (frames[i].hasAttribute(DataName)) {
-            let topScrollHeight = i * Math.abs(zDepthDistance) / Math.abs(zChangeSpeedMultiplier);
-
-            if (isTheLastFrame) {
-               // расстояние между последним и предпоследним фреймом больше на значение zDepthDistance
-               return topScrollHeight += Math.abs(zDepthDistance)
-            }
-            return topScrollHeight
+         if (frames[i].hasAttribute(dataName)) {
+            let topScrollHeight = i * Math.abs(Z_DEPTH_DISTANCE) / Math.abs(Z_CHANGE_SPEED_MULTIPLIER);
+            return (
+               isTheLastFrame
+                  ? topScrollHeight += Math.abs(Z_DEPTH_DISTANCE)
+                  : topScrollHeight
+            );
          }
       }
-   }
-}
+   },
+};
+
+navLinks.forEach((navLink) => {
+   navLink.addEventListener('click', () => navScrollData.scrollToFrame(navLink))
+});
 
 
-const logoSpan = document.querySelector('.logo__span');
-const mainNav = document.querySelector('.main-nav');
-const menuButton = document.querySelector('.menu-button');
-const pageFooter = document.querySelector('.page-footer');
-
-menuButton.onclick = function () {
-   let isMenuNotActive = !logoSpan.classList.contains('logo__span_active');
-
-   if (isMenuNotActive) {
-      logoSpan.classList.add('logo__span_active');
-      pageFooter.style.setProperty('--footer-translateY-active', 0);
-      mainNav.style.setProperty('--_nav-translateY-active', 0);
-      menuButtonActiveProperties.setActiveProperties(menuButton);
-   } else {
-      logoSpan.classList.remove('logo__span_active');
-      pageFooter.removeAttribute('style');
-      mainNav.removeAttribute('style');
-      menuButton.removeAttribute('style');
-   }
-}
-
-let menuButtonActiveProperties = {
+const menuButtonActive = {
    '--_clr-active': 'var(--clr-bg-dark)',
    '--_shadow-offest-active': 0,
    '--_top-position-active': '50%',
    '--before-rotate-angle': '225deg',
    '--after-rotate-angle': '315deg',
 
-   setActiveProperties: function (element) {
+   setActiveProperties(element) {
       for (let key in this) {
          element.style.setProperty(key, this[key]);
       }
+   },
+};
+
+function toogleMenuButton(evt) {
+   const isMenuNotActive = !logoSpan.classList.contains('logo__span_active');
+
+   if (isMenuNotActive) {
+      logoSpan.classList.toggle('logo__span_active');
+      pageFooter.style.setProperty('--footer-translateY-active', 0);
+      mainNav.style.setProperty('--_nav-translateY-active', 0);
+      menuButtonActive.setActiveProperties(evt.target);
+   } else {
+      logoSpan.classList.toggle('logo__span_active');
+      pageFooter.removeAttribute('style');
+      mainNav.removeAttribute('style');
+      menuButton.removeAttribute('style');
    }
 }
 
-
-document.onreadystatechange = function () {
-   if (document.readyState === 'complete') {
-      setTimeout(() => {
-         // запуск onscroll логики
-         window.scrollTo(0, 1);
-         window.scrollTo(0, 0);
-      }, 0);
-      setTimeout(function () {
-         loader.style.display = 'none';
-         main.style.opacity = 1;
-      }, 1000)
-   }
-}
-
+menuButton.addEventListener('click', toogleMenuButton);
